c9f9b59a1ee9eeb39379a3ed1366fc7b11faadd6
Igor Khromov <khromov.igor@gmail.com>
2021-09-26 00:42:21 +0300

    Added some missed files

---
+++ src/main/java/com/igorkhromov/portsadapters/PortsAdaptersApplication.java
@@ -0,0 +1,13 @@
+package com.igorkhromov.portsadapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortsAdaptersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortsAdaptersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorkhromov/portsadapters/PortsAdaptersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorkhromov.portsadapters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortsAdaptersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
